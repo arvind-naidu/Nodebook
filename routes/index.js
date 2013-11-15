d49671cc7034f2a91ee05e3d918cfb132d66592a
unknown <trmaynard@ITSWEB-TRMAYNA.HS.wvu-ad.wvu.edu>
2013-11-15 15:06:26 -0500

    Added new message form and another styles.

--- routes/index.js
+++ routes/index.js
@@ -9,42 +9,42 @@ exports.index = function(req, res){
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
-            "name": "Jack",
+            "name": "Jack Dehlin",
             "entryDate": "12/12/12",
             "status": true
         },
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
-            "name": "Kent",
+            "name": "Kent Pirlo",
             "entryDate": "12/12/12",
             "status": true
         },
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan.",
             "email": "test@gmail.com",
-            "name": "Libby",
+            "name": "Libby DeHaan",
             "entryDate": "12/12/12",
             "status": true
         },
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
-            "name": "Rose",
+            "name": "Rose McCluskey",
             "entryDate": "12/12/12",
             "status": true
         },
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
-            "name": "Tanner",
+            "name": "Tanner Yuhas",
             "entryDate": "12/12/12",
             "status": true
         },
         {
             "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
-            "name": "Travis",
+            "name": "Travis Maynard",
             "entryDate": "12/12/12",
             "status": true
         }
