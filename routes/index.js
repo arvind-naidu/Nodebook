c477c0a5e8c01827e61fcc2f352bed54e58dfd4e
unknown <trmaynard@ITSWEB-TRMAYNA.HS.wvu-ad.wvu.edu>
2013-11-15 13:55:49 -0500

    Re-organzed library files and finished initial design on messages.

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ exports.index = function(req, res){
   var messages = {
     "guestMessages": [
         {
-            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
             "name": "Jack",
             "entryDate": "12/12/12",
@@ -21,14 +21,14 @@ exports.index = function(req, res){
             "status": true
         },
         {
-            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan.",
             "email": "test@gmail.com",
             "name": "Libby",
             "entryDate": "12/12/12",
             "status": true
         },
         {
-            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Sed eleifend, justo ac bibendum tincidunt, nunc libero ultricies odio, eu bibendum lectus magna ac lectus. Nunc elementum pharetra elit at accumsan. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            "message": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus mi ac sapien blandit luctus. Duis et nisi lectus. Donec eleifend tempor elit nec molestie. Donec blandit congue augue consequat scelerisque. Suspendisse ultricies convallis ante, ut lacinia elit lacinia sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
             "email": "test@gmail.com",
             "name": "Rose",
             "entryDate": "12/12/12",
